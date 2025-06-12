0360d5dbb8843c98afa3bad24f7ff252f511956d
RamotepIFPB <ramoteplopes@gmail.com>
2025-06-12 12:43:01 -0300

    Update main.js

--- Ore-Miner/src/main.js
+++ Ore-Miner/src/main.js
@@ -1,13 +1,13 @@
 
 const estado = {
-  porta: false
+  refresh: false
 };
 
 const observado = new Proxy(estado, {
   set(obj, prop, value) {
     obj[prop] = value;
 
-    if (prop === "porta") {
+    if (prop === "refresh") {
       if (value === true) {
         console.log("A porta está aberta");
       } else {
@@ -21,14 +21,37 @@ const observado = new Proxy(estado, {
 
 const botao = document.getElementById("start");
 const description = document.getElementById("description");
+const highscore = document.getElementById("highscore");
+const minerio = document.getElementById("minerio");
+const minerio_img = document.querySelector("#minerio img");
+const score = document.getElementById("score")
+var scorenumber = 0
 
 botao.addEventListener("click", function () {
   description.style.display = "none";
+  botao.style.display = "none";
+  highscore.style.display = "none"
+  minerio.style.display = "block";
+  minerio_img.style.display = "block";
+  score.style.display = "block";
   setTimeout(function () {
     description.style.display = "block";
+    botao.style.display = "inline-block";
+    highscore.style.display = "block"
+    minerio.style.display = "none";
+    minerio_img.style.display = "none";
+    score.style.display = "none";
+    scorenumber = 0
+    score.textContent = "SCORE: " + scorenumber;
   }, 10000)
 });
 
+minerio.addEventListener("click", function () {
+  scorenumber = scorenumber + 10
+  score.textContent = "SCORE: " + scorenumber;
+  console.log(scorenumber)
+})
+
 // Testando:
-observado.porta = true;  // 👉 "A porta está aberta"
-observado.porta = false; // 👉 "A porta está fechada"
+observado.refresh = true;  // 👉 "A porta está aberta"
+observado.refresh = false; // 👉 "A porta está fechada"
