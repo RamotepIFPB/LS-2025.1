ae0674c74a99f1e4950403c3adaf1efc7a03bb9b
RamotepIFPB <ramoteplopes@gmail.com>
2025-06-13 01:11:33 -0300

    Update main.js

--- Ore-Miner/src/main.js
+++ Ore-Miner/src/main.js
@@ -25,7 +25,13 @@ const highscore = document.getElementById("highscore");
 const minerio = document.getElementById("minerio");
 const minerio_img = document.querySelector("#minerio img");
 const score = document.getElementById("score")
+var luck_upgrades = 1
+var speed_upgrades = 1
+var multiplier_upgrades = 1
+var m_atual = 3
 var scorenumber = 0
+import { carregar_Luck } from "./info-storage.js"
+import { carregar_Speed } from "./info-storage.js"
 
 botao.addEventListener("click", function () {
   description.style.display = "none";
@@ -34,6 +40,28 @@ botao.addEventListener("click", function () {
   minerio.style.display = "block";
   minerio_img.style.display = "block";
   score.style.display = "block";
+  const upgrade1 = Math.floor(Math.random() * 2) + 1;
+  const upgrade2 = Math.floor(Math.random() * 2) + 1;
+  const upgrade3 = Math.floor(Math.random() * 2) + 1;
+  if (upgrade1 == 1) {
+    carregar_Luck("pw-1", luck_upgrades)
+  }
+  if (upgrade1 == 2) {
+    carregar_Speed("pw-1", speed_upgrades)
+  }
+  if (upgrade2 == 1) {
+    carregar_Luck("pw-2", luck_upgrades)
+  }
+  if (upgrade2 == 2) {
+    carregar_Speed("pw-2", speed_upgrades)
+  }
+  if (upgrade3 == 1) {
+    carregar_Luck("pw-3", luck_upgrades)
+  }
+  if (upgrade3 == 2) {
+    carregar_Speed("pw-3", speed_upgrades)
+  }
+  
   setTimeout(function () {
     description.style.display = "block";
     botao.style.display = "inline-block";
@@ -49,7 +77,15 @@ botao.addEventListener("click", function () {
 let animationTimeout;
 
 minerio.addEventListener("click", function () {
-  scorenumber = scorenumber + 10;
+  if (m_atual == 3) {
+    scorenumber = scorenumber + 10;
+  }
+  if (m_atual == 2) {
+    scorenumber = scorenumber + 15;
+  }
+  if (m_atual == 1) {
+    scorenumber = scorenumber + 25;
+  }
   score.textContent = "SCORE: " + scorenumber;
   console.log(scorenumber);
   minerio_img.classList.remove('shake');
@@ -60,19 +96,24 @@ minerio.addEventListener("click", function () {
 
   const ore = Math.floor(Math.random() * 5) + 1;
   if (ore == 1) {
-    minerio_img.src = "../images/iron.png"
+    minerio_img.src = "../images/iron.png";
+    m_atual = 1;
   }
   if (ore == 2) {
-    minerio_img.src = "../images/coal.png"
+    minerio_img.src = "../images/coal.png";
+    m_atual = 2;
   }
   if (ore == 3) {
-    minerio_img.src = "../images/rock.png"
+    minerio_img.src = "../images/rock.png";
+    m_atual = 3;
   }
   if (ore == 4) {
-    minerio_img.src = "../images/rock.png"
+    minerio_img.src = "../images/rock.png";
+    m_atual = 3;
   }
   if (ore == 5) {
-    minerio_img.src = "../images/rock.png"
+    minerio_img.src = "../images/rock.png";
+    m_atual = 3;
   }
 
   clearTimeout(animationTimeout);
